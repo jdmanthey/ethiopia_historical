ef62080d7fbc0322536b4a6e35b2cfbb9fb0c00c
Joe Manthey <jdmanthey@gmail.com>
2024-06-16 15:56:24 -0500

    Create 20_count_genotypes_for_het.sh

---
+++ 24_test_depth/20_count_genotypes_for_het.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#SBATCH --chdir=./
+#SBATCH --job-name=het
+#SBATCH --nodes=1 --ntasks=1
+#SBATCH --partition quanah
+#SBATCH --time=48:00:00
+#SBATCH --mem-per-cpu=24G
+#SBATCH --array=1-8
+
+
+# define variables
+input=$( head -n${SLURM_ARRAY_TASK_ID} input_vcf_files.txt | tail -n1 )
+output=${input%.vcf}_het.txt
+
+cd /lustre/scratch/jmanthey/11_ethiopia_historical/08_hetb
+
+# count number of columns in vcf
+number=$( awk '{print NF; exit}' $input )
+
+# loop to get genotypes for each individual
+for i in $(seq 10 $number); do 
+sampname=$( head -n1 $input | cut -f $i );
+echo "${sampname}" >> $output;
+cut -f $i $input | cut -c1-3 | sort | uniq -c >> $output;
+done
+
+
+cd /lustre/scratch/jmanthey/11_ethiopia_historical/08_hetc
+
+# count number of columns in vcf
+number=$( awk '{print NF; exit}' $input )
+
+# loop to get genotypes for each individual
+for i in $(seq 10 $number); do 
+sampname=$( head -n1 $input | cut -f $i );
+echo "${sampname}" >> $output;
+cut -f $i $input | cut -c1-3 | sort | uniq -c >> $output;
+done
